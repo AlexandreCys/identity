ced84646e5485eff9413b18dba2907f3294cdf40
Alexandre Cys <alexandre.cys@qover.me>
2017-09-20 16:24:58 +0200

    Add identity server

---
+++ server.js
@@ -0,0 +1,9 @@
+const io = require('socket.io-client');
+const socket = io.connect(`${config.base.url}:${config.base.port}`, {
+  reconnect: true,
+  query: "type=authenticate",
+});
+
+socket.on('connect', () => { 
+  console.log('Authenticate service connected')
+});
